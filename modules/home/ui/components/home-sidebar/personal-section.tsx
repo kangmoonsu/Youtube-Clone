d11496115171b115836c9c5bf61309be098edbf8
michael <moonsu1011@naver.com>
2025-12-19 14:28:57 -0600

    feat: Add home layout, sidebar navigation, and navbar components

---
+++ modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
+import Link from "next/link";
+import {
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuItem,
+    SidebarMenuButton,
+    SidebarGroupLabel
+} from "@/components/ui/sidebar";
+
+const items = [
+    {
+        title: "History",
+        url: "/playlists/history",
+        icon: HistoryIcon,
+        auth: true,
+    },
+    {
+        title: "Liked videos",
+        url: "/playlists/liked-videos",
+        icon: ThumbsUpIcon,
+        auth: true,
+    },
+    {
+        title: "All playlists",
+        url: "/playlists",
+        icon: ListVideoIcon,
+        auth: true,
+    },
+];
+
+export const PersonalSection = () => {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>You</SidebarGroupLabel>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton
+                                tooltip={item.title}
+                                asChild
+                                isActive={false}
+                                onClick={() => { }}>
+                                <Link href={item.url} className="flex items-center gap-4">
+                                    <item.icon />
+                                    <span className="text-sm">{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    );
+};
